988b541fddcd84dd7fd4d9ae9158a15abdf9db71
MazzGuille <mazzcode@gmail.com>
2022-05-25 02:37:00 -0300

    incomplete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ====================================================MAIN START======================================================= //
+WriteLine("Hello, World!");
+
+
+
+// ====================================================MAIN START======================================================= //
+
+
+// ====================================================CLASS CONTROL START======================================================= //
+class ManejoDeArchivos
+{
+    StreamReader archivo = null;
+
+    int contador = 0;
+
+    string linea;
+
+    public ManejoDeArchivos()
+    {
+        archivo = new StreamReader(@"C:\Users\HP-ProBook6460B\Desktop\programing\tarea.txt");
+
+        while ((linea = archivo.ReadLine()) != null) ;
+        {
+            WriteLine(linea);
+
+            contador++;
+        }
+
+        {
+
+        }
+    }
+
+    public void Mensaje()
+    {
+        WriteLine($"Con el $: Hay {contador} lineas");
+        WriteLine();
+        WriteLine("Sin el $: Hay {0} lineas", contador);
+    }
+}
+// ====================================================CLASS CONTROL END======================================================= //
+
+
+
+// ====================================================DESTRUCTOR START======================================================= //
+
+
+
+
+// ====================================================DESTRUCTOR END======================================================= //
